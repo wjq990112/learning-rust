16e9070ecfcf6cf2dcc5c964dadc4ef319f30ea1
wjq990112 <1163585385@qq.com>
2023-05-13 16:51:48 +0800

    types

---
+++ src/types.rs
@@ -0,0 +1,38 @@
+/*
+Primitive Types--
+Integers: u8, 18, u16, 116， u32, i32, u64, 164, u128, 1128 (number of bits they take in
+memory)
+Floats: 32, f64
+Boo lean (bool)
+Characters (char)
+Tup les
+Arrays
+*/
+
+// Rust is a statically typed language, which means that it must know the types of all variables at compile time, however, the compiler can usually infer what type we want to use based on the value and how we use it.
+
+pub fn run() {
+    // Default is "i32"
+    let x = 1;
+
+    // Default is "f64"
+    let y = 2.5;
+
+    // Add explicit type
+    let z: i64 = 727272727272;
+
+    // Find max size
+    println!("Max i32: {}", std::i32::MAX);
+    println!("Max i64: {}", std::i64::MAX);
+
+    // Boolean
+    let is_active: bool = true;
+
+    // Get boolean from expression
+    let is_greater: bool = 10 < 5;
+
+    let a1 = 'a';
+    let face = '\u{1F600}';
+
+    println!("{:?}", (x, y, z, is_active, is_greater, a1, face));
+}
